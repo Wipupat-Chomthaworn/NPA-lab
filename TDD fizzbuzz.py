fff9151cfd13cb1cd7c1a3128c6fa8405a6cb322
64070103 Wipupat Chomthaworn <88475411+Wipupat-Chomthaworn@users.noreply.github.com>
2024-02-02 10:01:15 +0700

    5 test case

--- TDD fizzbuzz.py
+++ TDD fizzbuzz.py
@@ -1,12 +1,19 @@
 def test_fizzbuzz():
     assert fizzBuzz(1) == 1
     assert fizzBuzz(2) == 2
+    assert fizzBuzz(3) == "Fizz"
+    assert fizzBuzz(4) == 4
+    assert fizzBuzz(5) == "Buzz"
+    print("TestPass")
 
 
-def fizzBuzz(i):
-    if (i %3 == 0):
-        return "fizz"
-    return 1
+def fizzBuzz(num):
+    if (num ==3):
+        return "Fizz"
+    elif (num % 5 ==0):
+        return "Buzz"
+
+    return num
 
 test_fizzbuzz()
 print("Process", "Done!")
